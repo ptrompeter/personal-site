70d8c68b05b2de68b26256c08dff7ca3d3e4261f
ptrompeter <patrick.trompeter@gmail.com>
2018-06-13 12:39:02 -0700

    Added an about paragraph, basic show and hide functionality.

--- js/app.js
+++ js/app.js
@@ -4,13 +4,18 @@ $(document).ready(function(){
         if (!html.hasClass("background-tint")) {
             html.addClass("background-tint");
         }
+        $("article").hide();
+        $("#about-me").show();
     });
     $("#portfolio").click(function () {
         if(!html.hasClass("background-tint")) {
             html.addClass("background-tint");
         }
+        $("article").hide();
+        $("#portfolio").show();
     });
     $("#home").click(function () {
+        $("article").hide();
         html.removeClass("background-tint");
     });
 });
